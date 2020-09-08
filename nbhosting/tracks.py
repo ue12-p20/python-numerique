38dce455b1ff284e89cb5e3573290e05cd402330
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-09-08 10:27:45 +0200

    tweaked toc - full contents

--- nbhosting/tracks.py
+++ nbhosting/tracks.py
@@ -13,30 +13,35 @@ def tracks(coursedir):
     result is a list of Track instances
     """
 
+    # tentative numbering
+    # 00-19 for course
+    # 50-69 for exos
+    # 80-[89]9 for quiz and the like
+
     track_specs = [
         ('cours #1: intro & install' , 'survol & mise en place des outils', 'intro',
          [ 
            ('introduction', 'notebooks/0-[0-8]*.md'),
-           ('checklist', 'notebooks/0-9*.md'),
+           ('checklist', 'notebooks/0-[89]*.md'),
          ]),
-        ('cours #2: python' , 'survol du langage Python', 'python',
+        ('cours #2: Python' , 'survol du langage Python', 'python',
          [ 
-           ('les bases', 'notebooks/1-[0-8]*.md'),
-           ('quiz', 'notebooks/1-9*.md'),
+           ('Python primer', 'notebooks/1-[01]*.md'),
+           ('exos', 'notebooks/1-[56]*.md'),
          ]),
         ('cours #3: numpy' , 'programmation vectorielle', 'numpy',
          [ 
-           ('numpy', 'notebooks/2-[0-8]*.md'), 
-           ('quiz', 'notebooks/2-9*.md'), 
+           ('quiz Python', 'notebooks/1-[89]*.md'),
+           ('numpy', 'notebooks/2-[01]*.md'), 
+           ('exos', 'notebooks/2-[56]*.md'), 
+           ('quiz', 'notebooks/2-[89]*.md'), 
         ]),
-        ('cours #4: dataviz & pandas' , 'outils pour la data science', 'dataviz',
+        ('cours #4: pandas & dataviz' , 'outils pour la data science', 'datascience',
          [ 
-           ('dataviz & pandas', 'notebooks/[34]-[0-8]*.md'),
-           ('quiz', 'notebooks/[34]-9*.md'),
+           ('pandas & dataviz', 'notebooks/[34]-[01]*.md'),
+           ('exos', 'notebooks/[34]-[56]*.md'),
+           ('quiz', 'notebooks/[34]-[89]*.md'),
          ]),
-#        ('évaluation', 'évaluation', 'eval',
-#         [ ('évaluation', 'notebooks/4-[0-9]*.md'),
-#         ])
         ]
 
     return [Track(coursedir, 
